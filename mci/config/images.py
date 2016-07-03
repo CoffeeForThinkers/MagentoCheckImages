4ac32c35c82a0450c2194c6482005985c32e5b6f
Dustin Oprea <myselfasunder@gmail.com>
2016-07-03 15:31:50 +0000

    We can now check against a lcoal filepath if available.

--- mci/config/images.py
+++ mci/config/images.py
@@ -1,1 +1,7 @@
+import os
+
 PROGRESS_INTERVAL_S = 10
+MAGENTO_PATH = os.environ.get('MAGENTO_PATH')
+
+MINIMUM_IMAGE_SIZE_B = 1000
+
--- mci/images.py
+++ mci/images.py
@@ -1,10 +1,12 @@
 import logging
 import requests
 import collections
+import os
 
 import ma.api.product
 import ma.api.media
 
+import mci.constants
 import mci.config.images
 import mci.progress
 
@@ -12,11 +14,54 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class Images(object):
+    def __init__(self):
+        self.__use_path = bool(mci.config.images.MAGENTO_PATH)
+
     def images_gen(self, product_id):
         ma_ = ma.api.media.MediaApi()
         images = ma_.get_list_with_product_id(product_id)
 
         return images
+
+    def __check_image_via_url(self, image):
+        url = image['url']
+
+        r = requests.head(url, stream=True)
+
+        try:
+            r.raise_for_status()  
+        except requests.HTTPError:
+            _LOGGER.warning("Not found: (%d) [%s] [%s]",
+                            i, s, image['label'])
+
+            return mci.constants.IE_DOES_NOT_EXIST
+        else:
+            len_ = int(r.headers['Content-Length'])
+            if len_ < mci.config.images.MINIMUM_IMAGE_SIZE_B:
+                _LOGGER.warning("Found but too small: (%d) [%s] [%s]",
+                                i, s, image['label'])
+
+                return mci.constants.IE_TOO_SMALL
+
+        return None
+
+    def __check_image_via_path(self, image):
+        filepath = os.path.join(mci.config.images.MAGENTO_PATH, 'media/catalog/product' + image['file'])
+        
+        if os.path.exists(filepath) is False:
+            return mci.constants.IE_DOES_NOT_EXIST
+
+        s = os.stat(filepath)
+        if s.st_size < mci.config.images.MINIMUM_IMAGE_SIZE_B:
+            return mci.constants.IE_TOO_SMALL
+
+        return None
+
+    def __check_image(self, image):
+        if self.__use_path is True:
+            return self.__check_image_via_path(image)
+        else:
+            return self.__check_image_via_url(image)
 
     def bad_images_gen(self):
         pa = ma.api.product.ProductApi()
@@ -35,24 +80,21 @@ class Images(object):
             s = product['sku']
             i = product['product_id']
 
-            _LOGGER.debug("Reading images for product (%d/%d): (%d) [%s]", 
-                          pi + 1, product_len, i, s)
+#            _LOGGER.debug("Reading images for product (%d/%d): (%d) [%s]", 
+#                          pi + 1, product_len, i, s)
 
             images = ma_.get_list_with_product_id(i)
             image_len = len(images)
 
             for ii, image in enumerate(images):
-                url = image['url']
-
-                _LOGGER.debug("Checking image (%d/%d) for (%d) [%s]: [%s]", 
-                              ii + 1, image_len, i, s, url)
-
-                r = requests.head(url, stream=True)
-
-                try:
-                    r.raise_for_status()
-                except requests.HTTPError:
+                error = self.__check_image(image)
+                if error == mci.constants.IE_DOES_NOT_EXIST:
                     _LOGGER.warning("Not found: (%d) [%s] [%s]", 
+                                    i, s, image['label'])
+
+                    yield (i, s, image, ())
+                elif error == mci.constants.IE_TOO_SMALL:
+                    _LOGGER.warning("Found but too small: (%d) [%s] [%s]",
                                     i, s, image['label'])
 
                     yield (i, s, image, ())
